b0bc14c59bd43c2714e7dcff60891ad1bf84f49c
ArtNov90 <art.novara@gmail.com>
2024-04-10 13:19:02 +0200

    TP autonomie CERCLEFACTORY

--- src/fr/diginamic/essais/TestCercle.java
+++ src/fr/diginamic/essais/TestCercle.java
@@ -6,14 +6,17 @@ public class TestCercle {
 	public static void main(String[] args) {
 		Cercle cercle1 = new Cercle(5.0);
 		Cercle cercle2 = new Cercle(7.5);
+		
+		afficherCercle(cercle1);
+        afficherCercle(cercle2);
+	}
 
-		System.out.println("Cercle 1 :");
-		System.out.println("Périmètre : " + cercle1.calculerPerimetre());
-		System.out.println("Surface : " + cercle1.calculerSurface());
-
-		System.out.println("\nCercle 2 :");
-		System.out.println("Périmètre : " + cercle2.calculerPerimetre());
-		System.out.println("Surface : " + cercle2.calculerSurface());
+        public static void afficherCercle(Cercle cercle) {
+            System.out.println("Rayon du cercle : " + cercle.getRayon());
+            System.out.println("Surface du cercle : " + cercle.calculerSurface());
+            System.out.println("Périmètre du cercle : " + cercle.calculerPerimetre());
+            System.out.println();
+        
 
 	}
 
