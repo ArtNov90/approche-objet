cdf66a8b7e7b256277fd739b0d83a27025c94af4
ArtNov90 <art.novara@gmail.com>
2024-04-23 11:04:25 +0200

    MAJ exercice fichier

--- src/fichier/LectureFichier.java
+++ src/fichier/LectureFichier.java
@@ -4,40 +4,23 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-
-import tri.Ville;
 
 public class LectureFichier {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)throws IOException {
 
 		Path path = Paths.get("C:\\Users\\artno\\OneDrive\\Bureau\\recensement.csv");
-		List<String> lines = Files.readAllLines(path);
-
-		ArrayList<Ville> listeVilles = new ArrayList<>();
-
-		// Parcourir chaque ligne du fichier
-		for (int i = 1; i < lines.size(); i++) {
-			String line = lines.get(i);
-			String[] tokens = line.split(";");
-
-			// Créer une instance de la classe Ville en extrayant les informations
-			Ville ville = new Ville();
-			ville.setNom(tokens[0]);
-			ville.setCodeDepartement((tokens[2]));
-			ville.setNomRegion(tokens[1]);
-			ville.setPopulationTotale(Integer.parseInt(tokens[9].replaceAll(" ", "")));
-
-			// Ajoute la ville à la liste des villes
-			listeVilles.add(ville);
+		List<String> lines = null;
+		try {
+			lines = Files.readAllLines(path);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
 		}
+		for (String line : lines) {
+            System.out.println(line);
+        }
 
-		// Affiche les informations sur toutes les villes
-		for (Ville ville : listeVilles) {
-			System.out.println(ville);
-		}
 	}
-
 }
